5fcf37012d2f7b366b818b27f34cf1b39a3cd955
Mona Lisa <waltongerman@gmail.com>
2022-06-06 09:49:43 -0300

    Desafio 40

---
+++ Desafio 40 - mejorar la arquitectura de nuestra API/src/models/factory.model.js
@@ -0,0 +1,23 @@
+const userModel = require("../models/user.model");
+const productModel = require("../models/product.model");
+const orderModel = require("../models/order.model");
+const cartModel = require("../models/cart.model");
+
+class ModelFactory {
+  static getModel(modelName) {
+    switch (modelName) {
+      case "user":
+        return userModel;
+      case "cart":
+        return cartModel;
+      case "product":
+        return productModel;
+      case "order":
+        return orderModel;
+      default:
+        throw new Error("Model doesnt exit");
+    }
+  }
+}
+
+module.exports = ModelFactory;
---
+++ Desafio 40 - mejorar la arquitectura de nuestra API/src/routes/cart.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { Router } = express;
+const router = Router();
+const controller = require("../controllers/cart.controller");
+
+router.get("/", controller.getAllCarts);
+
+router.get("/:id/products", controller.getCartProducts);
+
+router.get("/:id", controller.getCartById);
+
+router.post("/", controller.createCart);
+
+router.post("/:id/:productId", controller.addToCart);
+
+router.delete("/:id", controller.deleteCartById);
+
+router.delete("/:id/products/:prodCode", controller.deleteCartProductById);
+
+module.exports = router;
